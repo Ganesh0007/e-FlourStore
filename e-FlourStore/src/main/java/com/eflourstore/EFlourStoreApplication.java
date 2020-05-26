dcc4af5eba7b92cf350a04a03957799822310434
Ganesh Radje <radjeganesh@hotmail.fr>
2020-05-26 11:43:09 +0200

    BaseE-FlourStore

---
+++ e-FlourStore/src/main/java/com/eflourstore/EFlourStoreApplication.java
@@ -0,0 +1,13 @@
+package com.eflourstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFlourStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFlourStoreApplication.class, args);
+	}
+
+}
---
+++ e-FlourStore/src/test/java/com/eflourstore/EFlourStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eflourstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFlourStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
